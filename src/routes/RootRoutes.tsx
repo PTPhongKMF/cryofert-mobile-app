8ae2c26915fcf5ef06f16ce0b2fdd6bf29abb4a2
PTPhongKMF <169053895+PTPhongKMF@users.noreply.github.com>
2025-11-23 13:56:12 +0700

    feat: add error modal on refresh fail

--- src/routes/RootRoutes.tsx
+++ src/routes/RootRoutes.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router";
 import SplashScreen from "@src/pages/SplashScreen";
 import LandingTabRoutes from "@src/routes/LandingTabRoutes";
 import { ROUTES } from "@src/routes/routes";
 import AppTabRoutes from "@src/routes/AppTabRoutes";
-import { IonRouterOutlet } from "@ionic/react";
+import { IonRouterOutlet, useIonRouter } from "@ionic/react";
 import TreatmentBooking from "@src/pages/TreatmentBooking";
 import UpdateAccount from "@src/pages/UpdateAccount";
 import TransactionHistory from "@src/pages/TransactionHistory";
@@ -12,8 +12,16 @@ import TreatmentDetail from "@src/pages/TreatmentDetail";
 import PaymentPortal from "@src/pages/PaymentPortal";
 import TestPage from "@src/pages/devs/TestPage";
 import TestPage2 from "@src/pages/devs/TestPage2";
+import { setGlobalPush } from "@src/services/navigation-service";
 
 export default function RootRoutes() {
+  const router = useIonRouter();
+
+  useEffect(() =>
+    setGlobalPush((path, direction) => {
+      router.push(path, direction);
+    })
+  );
   return (
     <IonRouterOutlet>
       <Route exact path={ROUTES.ROOT} component={SplashScreen} />
